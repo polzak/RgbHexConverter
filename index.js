a1f8dfa0cc820a4ee9ff418b9748234d9e7d09b1
polzak <hello.polzak@gmail.com>
2018-06-25 18:27:29 +0900

    added converting from RGB to Hex

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
-const converter = (hex) => {
-    if (hex.startsWith('#') && hex.length === 7) {
-        hex = hex.slice(1);
-    }
-    
-    let rCode, gCode, bCode;
+const converter = (color) => {
 
-    if (hex.length === 6) {
-        rCode = hex.slice(0,2);
-        gCode = hex.slice(2,4);
-        bCode = hex.slice(4,6);
-        rCode = parseInt(rCode, 16);
-        gCode = parseInt(gCode, 16);
-        bCode = parseInt(bCode, 16);
+    let rgbCode;
+    let hexCode;
+
+    if (color.includes(',')) {
+        rgbCode = color.split(',');
+    } else if (color.includes(' ')) {
+        rgbCode = color.split(' ');
     }
+
+    if (rgbCode) {
+        rgbCode = rgbCode.map(el => el.trim());
+
+        if (rgbCode.length > 3) {
+            rgbCode = rgbCode.filter(el => el.length > 0);
+        } else if (rgbCode.length < 3) {
+            return 'You need to provide 3 numbers for Red, Green, and Blue.';
+        } 
+        console.log(rgbCode);
+        rgbCode = (rgbCode.map(el => Number(el))).filter(el => el < 256);
+
+        if (rgbCode.length < 3) {
+            return 'You need to provide each number less than 256.';
+        }
+
+        console.log(rgbCode);
+
+        rgbCode = rgbCode.map(el => el.toString(16));
+        const hexResult = '#' + rgbCode.join('');
+
+        return hexResult;
+    }
+
+
+
+    // if (hex.startsWith('#')) {
+    //     hex = hex.slice(1);
+    // }
     
-    return `rgb(${rCode}, ${gCode}, ${bCode})`;
+    // let rCode, gCode, bCode;
+
+    // if (hex.length === 6) {
+    //     rCode = hex.slice(0,2);
+    //     gCode = hex.slice(2,4);
+    //     bCode = hex.slice(4,6);
+    //     rCode = parseInt(rCode, 16);
+    //     gCode = parseInt(gCode, 16);
+    //     bCode = parseInt(bCode, 16);
+    // }
+    
+    // return `rgb(${rCode}, ${gCode}, ${bCode})`;
+
 }
 
-console.log(converter('#ab91cd'));
+const hex = '#ab91cd';
+const rgb = '220, 233, 219';
+
+console.log(converter(rgb));
